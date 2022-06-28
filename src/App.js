ab5eea6e95fd7ef820a0b4c5598949070d0a0fff
rakibul-wdp <rakibul.wdp@gmail.com>
2022-06-28 12:52:40 +0600

    add navbar in home page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import Home from './Pages/Home/Home/Home';
+import Navbar from './Pages/Shared/Navbar/Navbar';
+import { Routes, Route } from 'react-router-dom';
+import Product from './Pages/Home/Product/Product';
+
 function App() {
   return (
     <div>
-      <button class='btn btn-primary'>Button</button>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='product' element={<Product />} />
+      </Routes>
     </div>
   );
 }
